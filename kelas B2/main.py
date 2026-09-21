47a439f9c96361b41012cfb50b27d9b5da041fd3
robin <bagaskararobin@gmail.com>
2026-09-21 22:18:50 +0800

    perbaikan

---
+++ kelas B2/main.py
@@ -0,0 +1,66 @@
+nama = "robin"
+
+print(nama)
+
+nama_panjang = "robin setiawan"
+
+print(nama_panjang)
+
+angka_int1 = 10
+angka_int2 = 12
+
+print(angka_int1 + angka_int2)
+
+a = 1.53
+b = 7.2
+
+print(a + b)
+
+nama = "Larry"
+hobi = "ngoding"
+print(nama)
+print(hobi)
+
+print('''Halo saya Rendy
+Saya suka belajar coding
+Hari ini saya belajar bahasa pemrograman Python''')
+
+teks = "rahmat flowchart"
+
+print(teks[3])
+print(teks[0:6])
+
+Hujan = False
+Panas = True
+
+print(Hujan)
+
+data = ["APD", 10, True]
+
+print(data[1])
+print(data[1:3])
+
+data_mahasiswa = ("Rafi","034",True, 3.5)
+print(data_mahasiswa[0])
+print(data_mahasiswa[2])
+
+mahasiswa = {
+    "nim": "21",
+    "nama": "robin"
+}
+
+print(mahasiswa["nama"])
+
+angka = 1
+angka = str(angka)
+print(type(angka))
+
+nama = input("Masukkan nama anda: ")
+print(nama)
+
+a = True
+b = False
+
+c = a and b
+
+print(c)
